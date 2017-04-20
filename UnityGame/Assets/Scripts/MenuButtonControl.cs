a74d399ee4aa385837c32a0aae8584ea26790045
Ciara Sastre <sastreciara@hotmail.com>
2017-04-20 20:18:31 +0100

    Finished mechanics of Menu

---
+++ UnityGame/Assets/Scripts/MenuButtonControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonControl : MonoBehaviour {
+
+	//public string levelToLoad;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(0);
+		//Application.LoadLevel(0);
+
+	}
+
+	public void Controls()
+	{
+		SceneManager.LoadScene(4);
+		//Application.LoadLevel(4);
+
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+		//SceneManager.Quit();
+	}
+}
