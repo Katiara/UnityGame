9e72e2ef1ce5c074cc752556c5331c6a6e33661b
Ciara Sastre <sastreciara@hotmail.com>
2017-04-24 02:12:47 +0100

    Fixed camera + Removed Redudant code

--- UnityGame/Assets/Scripts/playerController.cs
+++ UnityGame/Assets/Scripts/playerController.cs
@@ -7,20 +7,18 @@ using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
 
+	//Character Control
 	public float moveSpeed;
 	public float rotationSpeed;
 	private CharacterController cc;
 
-	Vector3 movement;
-	private Rigidbody rb;
+	//In game Text
 	private int carrotnum;
 	public Text countText;
 	public Text winText;
 
 	void Start ()
 	{
-		//rb = GetComponent<Rigidbody>();
-
 		cc = GetComponent<CharacterController> ();
 
 		//Carrot collection and counter code
@@ -31,34 +29,22 @@ public class playerController : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-		float moveVertical = Input.GetAxisRaw ("Vertical");
-
-		cc.SimpleMove (Physics.gravity);
-
-		transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal") * rotationSpeed *Time.deltaTime,0) );
-
-		Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) *moveSpeed; // Vector(001)
-
-		cc.Move(forward* Time.deltaTime);
-		//Move player around scene
-		//Move(moveHorizontal, moveVertical);
-
-		//Turn();
+		//Move Horse Player around map
+		horseController();
 	}
 
 	 //This section moves the Player
-	//void Move () //float mH, float mV
-	//{
-		/*Set movement based on axis input
-		movement.Set(mH, 0f, mV);
+	void horseController()
+	{
+		//Firstly turn player around the Horizontal
+		cc.SimpleMove (Physics.gravity);
+		transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal") * rotationSpeed *Time.deltaTime,0) );
 
-		//Make it proportianal to speed per sec
-		movement = movement.normalized * speed * Time.deltaTime;
+		// Next get it to move forward on the vertical
+		Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) *moveSpeed; // Vector(0,0,1)
+		cc.Move(forward* Time.deltaTime);
+	}
 
-		// Move the player to it's current position plus the movement.
-		rb.MovePosition (transform.position + movement);*/
-	//}
 
 	//When player hits carrot, carrot will disappear
 	//Carrot counter will be increased
@@ -87,23 +73,4 @@ public class playerController : MonoBehaviour {
 		}
 	}
 
-
-	//This section turns the Player relative to the camera
-	//void Turn()
-	//{
-
-	//}
 }
-
-/*		//WORKS
-if(Input.GetKey(KeyCode.D))
-{
-	transform.Rotate (Vector3.up * speed * Time.deltaTime); //Turn in the same speed as move
-	rb.MovePosition (transform.position + movement);
-}
-
-if(Input.GetKey(KeyCode.A))
-{
-	transform.Rotate (Vector3.down * speed * Time.deltaTime); //Turn in the same speed as move
-	rb.MovePosition (transform.position + movement);
-}*/
