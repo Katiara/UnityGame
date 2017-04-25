830b507733b651fa89f78e8db78de8c4a5021c3a
Ciara Sastre <sastreciara@hotmail.com>
2017-04-25 20:16:52 +0100

    Added Glindas dialoge

---
+++ UnityGame/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+	public GameObject texBox;
+
+	public Text theText;
+
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine; // knowing where line Starts
+	public int endAtLine; // knowing where line ends
+
+	//public playerController Player;
+
+	void Start()
+	{
+		//Player = findObjectOfType<playerController> ();
+
+		//Check if text file is there
+		if (textFile != null)
+		{
+			textLines = (textFile.text.Split ('\n'));
+
+		}
+
+		if (endAtLine == 0) 
+		{
+			endAtLine = textLines.Length - 1;
+		}
+	}
+
+	void Update()
+	{
+		theText.text = textLines [currentLine];
+
+		if (Input.GetKeyDown (KeyCode.Return)) 
+		{
+			currentLine += 1;
+		}
+	}
+
+}
