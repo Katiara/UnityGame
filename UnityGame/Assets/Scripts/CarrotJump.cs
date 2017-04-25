e2df1d09e6a8359b52dc135ea4508fd9d2ddf17c
KatieObrien <katie-obrien96@hotmail.com>
2017-04-25 17:29:43 +0100

    Code changed

--- UnityGame/Assets/Scripts/CarrotJump.cs
+++ UnityGame/Assets/Scripts/CarrotJump.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarrotJump : MonoBehaviour {
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
 		transform.Rotate (new Vector3 (5, 0, 45) * Time.deltaTime);
-		
+
 	}
+
+		
+
 }
